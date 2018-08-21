c44396d3127e6216bb0fabcdfdf408873b393bf9
SamzFerg <SamzFerg@gmail.com>
2018-08-21 13:48:52 -0500

    Added Reference Class

---
+++ src/main/java/com/SamzFerg/Starquest/reference/Reference.java
@@ -0,0 +1,11 @@
+package com.SamzFerg.Starquest.reference;
+
+public class Reference
+{
+    public static final String MOD_ID = "starquest";
+    public static final String MOD_NAME = "StarQuest";
+    public static final String VERSION = "1.7.10-1.0";
+    public static final String CLIENT_PROXY = "com.SamzFerg.Starquest.proxy.ClientProxy";
+    public static final String SERVER_PROXY = "com.SamzFerg.Starquest.proxy.ClientProxy";
+
+}
